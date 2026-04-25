7e7c8c3f426c2725e0d6e7d38fa18247293d083c
Moraless0 <moraless06@hotmail.com>
2026-04-24 23:58:37 -0600

    fix: :bug: Bucle in menu

--- select_option.py
+++ select_option.py
@@ -6,28 +6,33 @@ from show_products import listar_productos
 
 def select_option():
 
-    show_menu()
-
-    opc = int(input("Ingrese la opcion deseada"))
-    
-
     while True:
+        show_menu()
+
         try:
+            opc = int(input("Ingrese la opcion deseada: "))
+
             if opc == 1:
                 registrar_producto()
+
             elif opc == 2:
                 actualizar_cantidad()
+
             elif opc == 3:
                 listar_productos()
+
             elif opc == 4:
                 print("O4")
+
             elif opc == 5:
                 print("O5")
+
             elif opc == 6:
-                print("O6")
+                print("Saliendo...")
                 break
+
             else:
-                print("Invalido")
-        
+                print("❌ Opción inválida")
+
         except Exception:
-            print("Error")
+            print("❌ Error, ingrese un número válido")
