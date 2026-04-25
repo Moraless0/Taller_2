18e6e94c2aec060c662d4981a948037ea962ec02
Moraless0 <moraless06@hotmail.com>
2026-04-24 23:10:32 -0600

    feat: :tada: Create select menu

---
+++ select_option.py
@@ -0,0 +1,26 @@
+
+def select_option():
+    
+    opc = int(input("Ingrese la opcion deseada"))
+    
+
+    while True:
+        try:
+            if opc == 1:
+                print("O1")
+            elif opc == 2:
+                print("O2")
+            elif opc == 3:
+                print("O3")
+            elif opc == 4:
+                print("O4")
+            elif opc == 5:
+                print("O5")
+            elif opc == 6:
+                print("O6")
+                break
+            else:
+                print("Invalido")
+        
+        except Exception:
+            print("Error")
