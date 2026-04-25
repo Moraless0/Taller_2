582a1971a1c097313283cb8ed19f327dfae85ada
Moraless0 <moraless06@hotmail.com>
2026-04-24 23:09:18 -0600

    feat: :tada: Create menu

---
+++ show_menu.py
@@ -0,0 +1,16 @@
+
+def show_menu():
+    print("""
+============================
+        CHINO STORE        
+============================
+
+[1] Agregar producto
+[2] Listar productos
+[3] Actualizar cantidad
+[4] Eliminar producto
+[5] Calcular valor total del inventario
+[6] Salir
+
+============================
+""")
