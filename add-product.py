2bc1624e1d23d794aa0da7018a0dafb05532ab5c
Moraless0 <moraless06@hotmail.com>
2026-04-24 23:16:52 -0600

    feat: :tada: Create funcion add product

---
+++ add-product.py
@@ -0,0 +1,25 @@
+import json
+
+def registrar_producto():
+    nombre = input("Nombre del producto: ")
+    precio = float(input("Precio: "))
+    cantidad = int(input("Cantidad: "))
+
+    producto = {
+        "nombre": nombre,
+        "precio": precio,
+        "cantidad": cantidad
+    }
+
+    try:
+        with open("productos.json", "r") as f:
+            productos = json.load(f)
+    except:
+        productos = []
+
+    productos.append(producto)
+
+    with open("productos.json", "w") as f:
+        json.dump(productos, f, indent=4)
+
+    print("Producto guardado ✅")
