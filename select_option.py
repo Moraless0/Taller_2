a8ed65ef39a123f68e420630a5a74eb9e7c650d2
Moraless0 <moraless06@hotmail.com>
2026-04-24 23:53:07 -0600

    fix: :bug: Fix bug in funcions

--- select_option.py
+++ select_option.py
@@ -1,7 +1,13 @@
+from show_menu import show_menu
+
 from add_product import registrar_producto
+from edit_products import actualizar_cantidad
+from show_products import listar_productos
 
 def select_option():
-    
+
+    show_menu()
+
     opc = int(input("Ingrese la opcion deseada"))
     
 
@@ -10,9 +16,9 @@ def select_option():
             if opc == 1:
                 registrar_producto()
             elif opc == 2:
-                print("O2")
+                actualizar_cantidad()
             elif opc == 3:
-                print("O3")
+                listar_productos()
             elif opc == 4:
                 print("O4")
             elif opc == 5:
